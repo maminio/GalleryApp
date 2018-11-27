edd5bf6585a28dd1ea524f3d30e7904d2c7e7d19
Amin <moradi.amin74@gmail.com>
2018-11-27 14:53:29 +0000

    list shadow update

--- components/ImageHolder.js
+++ components/ImageHolder.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Image, Text, View, TouchableOpacity } from 'react-native';
 
 const ImageHolder = ({ author, id, imageWidth, margin, onPress })=>{
   return (
+    <View>
     <TouchableOpacity
       onPress={onPress}
-     style={{ margin }}>
+     style={{ margin,
+          shadowColor: 'black',
+          shadowOffset: { width: 1, height: 5 },
+          shadowOpacity: 0.3,
+          shadowRadius: 5,
+         }}
+    >
       <Image
         source={{ uri: `https://picsum.photos/200/300?image=${id}` }}
         style={{
           width: imageWidth,
           height: imageWidth,
           borderRadius: 15,
-          shadowColor: 'black',
-          shadowOffset: { width: 1, height: 5 },
-          shadowOpacity: 1,
-          shadowRadius: 5,
         }}
       />
-      <Text>
-        {author}
-      </Text>
+
     </TouchableOpacity>
+    <Text
+      style={{
+        fontWeight: 'bold',
+        marginTop: 0,
+        color: '#606663',
+        textAlign: 'center'
+       }}
+    >
+      {author}
+    </Text>
+    </View>
   )
 }
 
